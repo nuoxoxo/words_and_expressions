ac2ff42ef29cd80d3faf4f50644799386a727174
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-25 23:11:29 +0200

    done : local storage

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,24 @@ import { useState } from "react"
 import PrinterHSK from "./components/PrinterHSK"
 import PrinterIDM from "./components/PrinterIDM"
 
+
+
 function App() {
-  const [isSwitched, setIsSwitched] = useState<boolean>(true)
+  const storedIsSwitched = localStorage.getItem("isSwitched")
+
+  // OLD CODE
+  // const [isSwitched, setIsSwitched] = useState<boolean>(true)
+  const [isSwitched, setIsSwitched] = useState<boolean>(
+    storedIsSwitched ? JSON.parse(storedIsSwitched) : true
+  )
 
   const toggleIO = () => {
-    setIsSwitched((prevChoice: boolean) => !prevChoice)
+
+    // OLD CODE
+    // setIsSwitched((prevChoice: boolean) => !prevChoice)
+    const res = !isSwitched
+    setIsSwitched(res)
+    localStorage.setItem("isSwitched", JSON.stringify(res))
   }
 
   return (
